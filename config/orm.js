559aa32bec5ed0fc6fcfc997da0dcfeff4b1588a
Greeley Wentz <jasonhorowitz@Jasons-MacBook-Pro.local>
2019-02-22 19:23:34 -0500

    friday update

---
+++ config/orm.js
@@ -0,0 +1,87 @@
+var connection = require("./connection.js");
+
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
+
+function printQuestionMarks(num){
+    var arr = []
+    for (var i = 0; i < num; i++){
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function objToSql(ob){
+    var arr = [];
+for (var key in ob) {
+    var value = ob[key];
+    
+    if (Object.hasOwnProperty.call(ob, key)) {
+      
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      
+      arr.push(key + "=" + value);
+    }
+  }
+
+  return arr.toString();
+}
+
+
+var orm = {
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      console.log(result);
+
+    });
+    cb(result);
+  },
+  InsertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+}
+};
+
+module.exports = orm;
